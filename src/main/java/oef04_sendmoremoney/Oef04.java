1ecfe1ab08bc7fae9d8a1e298ae27fad64ace8cb
Koen Yskout <koen.yskout@kuleuven.be>
2026-04-27 15:01:42 +0200

    les 27/04

--- src/main/java/oef04_sendmoremoney/Oef04.java
+++ src/main/java/oef04_sendmoremoney/Oef04.java
@@ -38,13 +38,81 @@ public class Oef04 {
         }
 
         public PuzzleSolution solve() {
-            // TODO
+            return solve(new HashMap<>());
+        }
+
+        private PuzzleSolution solve(Map<Character, Integer> assignmentSoFar) {
+            var unassigned = new HashSet<>(allCharacters());
+            unassigned.removeAll(assignmentSoFar.keySet());
+
+            // if (startsWithZero(term1(), assignmentSoFar)) return null;
+            // if (startsWithZero(term2(), assignmentSoFar)) return null;
+            if (terms().stream().anyMatch(term -> startsWithZero(term, assignmentSoFar))) return null;
+            if (startsWithZero(total(), assignmentSoFar)) return null;
+
+            if (unassigned.isEmpty()) {
+                if (sumWith(assignmentSoFar) == replace(total(), assignmentSoFar)) {
+                    return new PuzzleSolution(Map.copyOf(assignmentSoFar));
+                } else {
+                    return null;
+                }
+            }
+
+            var nextChar = unassigned.iterator().next();
+            for (var digit = 0; digit < 10; digit++) {
+                if (!assignmentSoFar.containsValue(digit)) {
+                    assignmentSoFar.put(nextChar, digit);
+                    // 3.
+                    var solution = solve(assignmentSoFar);
+                    if (solution != null) {
+                        return solution;
+                    }
+                    assignmentSoFar.remove(nextChar);
+                }
+            }
+
             return null;
         }
 
+        private long sumWith(Map<Character, Integer> assignmentSoFar) {
+            // return replace(term1(), assignmentSoFar) + replace(term2(), assignmentSoFar);
+
+            return terms.stream()
+                    .mapToLong(term -> replace(term, assignmentSoFar))
+                    .sum();
+        }
+
         public Set<PuzzleSolution> solveAll() {
-            // TODO
-            return null;
+            return solveAll(new HashMap<>(), new HashSet<>());
+        }
+
+        private Set<PuzzleSolution> solveAll(Map<Character, Integer> assignmentSoFar, Set<PuzzleSolution> solutionsSoFar) {
+            var unassigned = new HashSet<>(allCharacters());
+            unassigned.removeAll(assignmentSoFar.keySet());
+
+            if (terms().stream().anyMatch(term -> startsWithZero(term, assignmentSoFar))) return solutionsSoFar;
+            if (startsWithZero(total(), assignmentSoFar)) return solutionsSoFar;
+
+            if (unassigned.isEmpty()) {
+                if (sumWith(assignmentSoFar) == replace(total(), assignmentSoFar)) {
+                    solutionsSoFar.add(new PuzzleSolution(Map.copyOf(assignmentSoFar)));
+                    return solutionsSoFar;
+                } else {
+                    return solutionsSoFar;
+                }
+            }
+
+            var nextChar = unassigned.iterator().next();
+            for (var digit = 0; digit < 10; digit++) {
+                if (!assignmentSoFar.containsValue(digit)) {
+                    assignmentSoFar.put(nextChar, digit);
+                    // 3. test of niet met nul start kan ook hier
+                    solveAll(assignmentSoFar, solutionsSoFar);
+                    assignmentSoFar.remove(nextChar);
+                }
+            }
+
+            return solutionsSoFar;
         }
 
         private long replace(String str, Map<Character, Integer> mapping) {
--- src/main/java/oef05_uurrooster/Oef05.java
+++ src/main/java/oef05_uurrooster/Oef05.java
@@ -1,6 +1,7 @@
 package oef05_uurrooster;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Oef05 {
 
@@ -12,12 +13,110 @@ public class Oef05 {
         PlanningParameters parameters();
     }
 
-    public record Course(String code, Set<String> people) {}
+    public record Course(String code, Set<String> people) {
+        public boolean overlappingPersonWith(Course other) {
+            return !Collections.disjoint(this.people(), other.people());
+            // return this.people().stream().anyMatch(p -> other.people().contains(p));
+        }
+    }
 
+    static class Planning implements IPlanning {
+
+        private final PlanningParameters parameters;
+        private final Map<Course, Integer> geplandeVakken = new HashMap<>();
+
+        public Planning(PlanningParameters parameters) {
+            this.parameters = parameters;
+        }
+
+        @Override
+        public int nbUsedSlots() {
+            return (int)geplandeVakken.values().stream().distinct().count();
+        }
+
+        @Override
+        public Set<Course> coursesInSlot(int slot) {
+//            var result = new HashSet<Course>();
+//            for (var course : geplandeVakken.keySet()) {
+//                if (geplandeVakken.get(course) == slot) {
+//                    result.add(course);
+//                }
+//            }
+//            return result;
+            return geplandeVakken.entrySet().stream()
+                    .filter(e -> e.getValue() == slot)
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toSet());
+        }
+
+        @Override
+        public PlanningParameters parameters() {
+            return parameters;
+        }
+
+        public void plan(Course vak, int slot) {
+            geplandeVakken.put(vak, slot);
+        }
+
+        public void unplan(Course vak, int slot) {
+            geplandeVakken.remove(vak);
+        }
+
+        public boolean canBePlannedInSlot(Course vak, int slot) {
+            // is er nog plaats?
+            if (coursesInSlot(slot).size() >= parameters.maxCoursesPerSlot()) return false;
+            // geen dubbele boekingen van personen
+            if (coursesInSlot(slot).stream().anyMatch(c -> c.overlappingPersonWith(vak))) return false;
+
+            return true;
+        }
+
+        public Planning copy() {
+            var copy = new Planning(parameters);
+            copy.geplandeVakken.putAll(this.geplandeVakken);
+            return copy;
+        }
+
+        public boolean isBetterThan(Planning other) {
+            if (this.nbUsedSlots() < other.nbUsedSlots()) return true;
+            if (this.nbUsedSlots() > other.nbUsedSlots()) return false;
+            // even veel slots
+            if (this.lastSlot() < other.lastSlot()) return true;
+
+            return false;
+        }
+
+        private int lastSlot() {
+            return geplandeVakken.values().stream().mapToInt(i -> i).max().orElse(0);
+        }
+    }
 
     public static IPlanning createPlanning(List<Course> vakken, PlanningParameters parameters) {
-        // TODO
-        return null;
+        return createPlanning(vakken, parameters, new Planning(parameters), null);
+    }
+
+    private static Planning createPlanning(List<Course> tePlannenVakken, PlanningParameters parameters, Planning planningSoFar, Planning bestSoFar) {
+        if (tePlannenVakken.isEmpty()) {
+            if (bestSoFar == null || planningSoFar.isBetterThan(bestSoFar)) {
+                return planningSoFar.copy();
+            }
+            return bestSoFar;
+        }
+
+        if (bestSoFar != null && bestSoFar.isBetterThan(planningSoFar)) return bestSoFar;
+
+        var vak = tePlannenVakken.getFirst();
+        var rest = tePlannenVakken.subList(1, tePlannenVakken.size());
+
+        for (var slot = 1; slot <= parameters.maxSlots(); slot++) {
+            if (planningSoFar.canBePlannedInSlot(vak, slot)) {
+                planningSoFar.plan(vak, slot);
+                bestSoFar = createPlanning(rest, parameters, planningSoFar, bestSoFar);
+                planningSoFar.unplan(vak, slot);
+            }
+        }
+
+        return bestSoFar;
     }
 
     static void main() {
